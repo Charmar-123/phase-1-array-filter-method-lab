e7699b095b703176957c730f7996854a052507c2
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-13 20:32:58 -0500

    commit tree 5c7e2af2143bb174ce05d6641a2d9babc6052e44

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+
+function findMatching (driversNames, name){
+    return driversNames.filter(item => item.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(driversNames, letters){
+    return driversNames.filter(function checkLetters(item){
+        for (let i = 0; i < letters.length; i++)
+            if (letters[i] === item[i]){
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+
+
+
+function matchName(driverObj, nameCheck){
+    return driverObj.filter(item => item.name === nameCheck)
+}
+
+
+
+
+
+
+
+
+
+
+// function checkLetters(){
+//     let i = 0;
+//     while(i < letters.length){
+//         if (letters[i] === driversNames.name[i]){
+//             i++
+
+//         } else {
+//             return false
+//         }
+//         return true
+//     }
+// }
